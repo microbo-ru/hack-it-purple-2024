c1c0c45e3ebc859010911b836c36fe2ddc82bd79
Aleksandr Davydenko <alexandrdavydenko@yandex.ru>
2024-03-11 00:17:47 +0300

    add worker restrictions + pretty printing results

--- libs/examples/min_cost.py
+++ libs/examples/min_cost.py
@@ -1,5 +1,6 @@
 from libs.model.resources.resource_assignment import MinCost
 
+
 # just for comments
 skills = [
     'Analysis',
@@ -7,30 +8,109 @@ skills = [
     'Testing'
 ]
 
-# name, duration_hrs, skill_required
+NUM_DAYS = 14
+
+# (name, effort_hrs, skill_required, start_day, end_day)
 tasks = [
-    ('Requirements Analysis', 10, 0),
-    ('API Programming', 20, 1),
-    ('API Testing', 5, 2)
+    ('Requirements Analysis', 6, 'analysis', 0, 0),
+    ('API Programming', 24, 'dev', 1, 4),
+    ('DB design', 8, 'dev', 1, 1),
+    ('unit-tests', 8, 'dev', 5, 5),
+    ('API Testing', 5, 'qa', 5, 5)
 ]
 
-# name, cost_hr, skill
+# (name, cost_hr, skill)
 resources = [
-    ('Analyst 1', 50.0, 0),
-    ('Analyst 2', 55.0, 0),
-    ('Dev', 60.0, 1),
-    ('SA', 80.0, 1),
-    ('QA1', 40.0, 2),
-    ('QA2', 40.0, 2),
+    ('Analyst 1', 50.0, 'analysis'),
+    ('Analyst 2', 55.0, 'analysis'),
+    ('Dev', 60.0, 'dev'),
+    ('SA', 80.0, 'dev'),
+    ('QA1', 40.0, 'qa'),
+    ('QA2', 40.0, 'qa'),
 ]
 
 model = MinCost(resources, tasks)
 
 solution = model.solve()
+# solution = {
+#   'task_assignments': {},
+#   'workers_assignments': {}
+# }
 
-for t in range(len(tasks)):
-    (worker, cost) = solution[t]
-    (task_name, *_) = tasks[t]
-    (worker_name, *_) = resources[worker]
+# print solutions
 
-    print(f'Task "{task_name}" is performed by "{worker_name}", cost = {cost}')
+__free = u' □ '
+__busy = u' ■ '
+__icon = [
+    u' ♥ ',
+    u' ♦ ',
+    u' ♣ ',
+    u' ♠ ',
+    u' ▲ ',
+    u' ▼ ',
+    u' © ',
+    u' * '
+]
+
+def print_task_assignments(short = False):
+    global t, task_name, _, worker_name
+
+    if short:
+        for t in range(len(tasks)):
+            (worker, cost) = solution['task_assignments'][t]
+            (task_name, *_) = tasks[t]
+            (worker_name, *_) = resources[worker]
+
+            print(f'Task "{task_name}" is performed by "{worker_name}", cost = {cost}')
+
+    if not short:
+        for t in range(len(tasks)):
+            (worker, cost) = solution['task_assignments'][t]
+            (task_name, *_, start_day, end_day) = tasks[t]
+            (worker_name, *_) = resources[worker]
+
+            day_icons = [__free for _ in range(NUM_DAYS)]  # empty row
+            for d in range(start_day, end_day + 1):
+                day_icons[d] = __busy
+            day_print = ''.join(day_icons)
+
+            task_print = '{:<25}'.format(task_name)
+
+            print(f'{task_print} : {day_print} : {worker_name} ({cost})')
+
+
+def print_worker_tasks(short = False, print_legend = True):
+    global worker_name, _, t, task_name
+
+    if short:
+        for w in range(len(resources)):
+            (worker_name, *_) = resources[w]
+            day_tasks = solution['workers_assignments'][w]
+            print(f'Worker "{worker_name}" is assigned following tasks: {day_tasks}')
+
+    if not short:
+        for w in range(len(resources)):
+            (worker_name, *_) = resources[w]
+
+            day_icons = [__free for _ in range(NUM_DAYS)]  # empty row
+            for (d, t) in solution['workers_assignments'][w]:
+                day_icons[d] = __icon[t]
+
+            day_print = ''.join(day_icons)
+            worker_print = '{:<25}'.format(worker_name)
+
+            print(f'{worker_print}: {day_print}')
+
+    if print_legend:
+        print('\n--- Legend ---')
+        for t in range(len(tasks)):
+            (task_name, *_) = tasks[t]
+            print(f'{__icon[t]}: {task_name}')
+
+
+print('\nTask assignments:\n')
+print_task_assignments()
+
+print('\nWork assignments:\n')
+print_worker_tasks()
+
--- libs/model/resources/resource_assignment.py
+++ libs/model/resources/resource_assignment.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
 
 from ortools.sat.python import cp_model
+
 
 class MinCost():
     def __init__(self,
@@ -12,39 +14,70 @@ class MinCost():
         self.num_workers = len(resources)
         self.num_tasks = len(tasks)
 
+        max_day = 0
+        for (*_, task_end) in tasks:
+            if task_end > max_day:
+                max_day = task_end
+
+        self.num_days = max_day + 1  # zero-based
 
     def solve(self):
         model = cp_model.CpModel()
+        __FALSE = model.NewConstant(False)
 
-        # Variables space
-        task_workers = {}
+        # Variables space:
+        # task_workers [d, t, w] -> days x tasks x workers == on what day D what worker W is assigned on task T
+        # task_workers = {}
+        task_workers = defaultdict(lambda: __FALSE)
+        # use default dict with None assinment to speed up iterations over tasks
 
         # Potentially every worker can do any task:
         for t in range(self.num_tasks):
-            (*_, t_skill) = self.tasks[t]
+            (*_, t_skill, t_start, t_end) = self.tasks[t]
 
-            for w in range(self.num_workers):
-                (*_, w_skill) = self.resources[w]
+            for d in range(t_start, t_end + 1):  # +1 - if the day end is the same as day start
+                for w in range(self.num_workers):
+                    (*_, w_skill) = self.resources[w]
 
-                if t_skill == w_skill:
-                    task_workers[t, w] = model.NewBoolVar(f'task{t}_worker{w}')
-                else:
-                    task_workers[t, w] = model.NewConstant(False)
+                    # by default = False
+                    if t_skill == w_skill:
+                        task_workers[d, t, w] = model.NewBoolVar(f'task{t}_worker{w}')
 
         # Constraints:
         # 1. Task is assigned to exactly one worker
         for t in range(self.num_tasks):
-            model.AddExactlyOne(task_workers[t, w] for w in range(self.num_workers))
+            (*_, t_start, t_end) = self.tasks[t]
+
+            # 1.1. For every day only 1 worker is assigned
+            for d in range(t_start, t_end + 1):
+                model.AddExactlyOne(task_workers[d, t, w] for w in range(self.num_workers))
+
+            # 1.2. If worker assigned on 1st day -> he should be assigned on the rest days as well
+            # otherwise could be, the task will be performed by different workers on different days
+            for w in range(self.num_workers):
+                model.AddBoolAnd(task_workers[d, t, w] for d in range(t_start, t_end + 1))\
+                     .OnlyEnforceIf(task_workers[t_start, t, w])
+
+                model.AddBoolAnd(task_workers[d, t, w].Not() for d in range(t_start, t_end + 1))\
+                     .OnlyEnforceIf(task_workers[t_start, t, w].Not())
+
+        # 2. Worker can do at most 1 task per day
+        for w in range(self.num_workers):
+            for d in range(self.num_days):
+                model.AddAtMostOne(task_workers[d, t, w] for t in range(self.num_tasks))
+
 
         # Objective function
         obj_task_costs = {}
         for t in range(self.num_tasks):
-            (_, task_duration_hrs, _) = self.tasks[t]
+            (_, task_effort_hrs, _, t_start, t_end) = self.tasks[t]
 
             assigned_workers_cost = []
             for w in range(self.num_workers):
                 (_, worker_cost_hr, _) = self.resources[w]
-                assigned_workers_cost.append(task_workers[t, w] * worker_cost_hr * task_duration_hrs)
+                # to simplify:
+                # just consider, if W works on t_start -> consider he is working the whole time as well
+                assigned_workers_cost.append(task_workers[t_start, t, w] * worker_cost_hr * task_effort_hrs)
 
             obj_task_costs[t] = sum(assigned_workers_cost)
 
@@ -56,12 +89,26 @@ class MinCost():
         if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
             print(f'Total cost = {solver.objective_value}\n')
 
-            solution = {}
+            solution = {
+                'task_assignments': {},
+                'workers_assignments': {}
+            }
+
             for t in range(self.num_tasks):
+                (*_, t_start, t_end) = self.tasks[t]
                 for w in range(self.num_workers):
-                    if solver.boolean_value(task_workers[t, w]):
+                    if solver.boolean_value(task_workers[t_start, t, w]):
                         # (worker_id, total_cost)
-                        solution[t] = (w, solver.Value(obj_task_costs[t]))
+                        solution['task_assignments'][t] = (w, solver.Value(obj_task_costs[t]))
+
+            for w in range(self.num_workers):
+                worker_solution = []
+                for d in range(self.num_days):
+                    for t in range(self.num_tasks):
+                        if solver.boolean_value(task_workers[d, t, w]):
+                            worker_solution.append((d, t))
+
+                solution['workers_assignments'][w] = worker_solution
 
             return solution
 
