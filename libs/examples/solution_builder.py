61fd13ab72a2e7ea33cd7f1d532734543d2f6640
Kocheryzhkin Vladimir <vkocherizhkin@gmail.com>
2024-03-13 23:47:29 +0300

    Update parent dates (#1)

--- libs/examples/solution_builder.py
+++ libs/examples/solution_builder.py
@@ -1,5 +1,5 @@
-
 from datetime import datetime, timedelta
+from pprint import pprint
 
 class SolutionBuilder:
     def reassign(self, data, tasks, resources, solution_task_assignments):
@@ -10,8 +10,6 @@ class SolutionBuilder:
             (start, finish, w) = solution_task_assignments[t]
             (task_name, *_) = tasks[t]
             (worker_name, *_) = resources[w]
-
-            # print(f'Task {task_name} is performed by {worker_name} start {start} end {finish}')
 
             assignment = next(t for t in data.Project.Assignments.Assignment if t.TaskUID == task_name)
             assignment.ResourceUID = worker_name
@@ -26,4 +24,38 @@ class SolutionBuilder:
             new_finish = project_start_date + timedelta(days = finish)
             task.Start = new_start.strftime(date_format)
             task.Finish = new_finish.strftime(date_format)
+
+    def update_parents_dates(self, data):
+        node_list = []
+        for i in data.Project.Tasks.Task:
+            node_list.append(i.OutlineNumber)
+
+        leaves_list = []
+        for s in node_list:
+            if not any([s in r for r in node_list if s != r]):
+                leaves_list.append(s)
+
+        parents_list = [t for t in node_list if t not in leaves_list]
+
+        tasks_with_dates = []
+        date_format = "%Y-%m-%dT%H:%M:%S"
+
+        for i in leaves_list:
+            task = next(t for t in data.Project.Tasks.Task if t.OutlineNumber == i)
+            tasks_with_dates.append((
+                task.OutlineNumber, 
+                datetime.strptime(task.Start, date_format),
+                datetime.strptime(task.Finish, date_format))
+            )
+
+        for i in parents_list:
+            task = next(t for t in data.Project.Tasks.Task if t.OutlineNumber == i)
+            q = task.OutlineNumber
+            all_sub_tasks = list(filter(lambda t: t[0].startswith(q), tasks_with_dates))
+
+            min_start = min(all_sub_tasks, key=lambda t: t[1])[1]
+            max_start = max(all_sub_tasks, key=lambda t: t[2])[2]
+
+            task.Start = min_start.strftime(date_format)
+            task.Finish = max_start.strftime(date_format)
 
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 from pprint import pprint
 from types import SimpleNamespace
 import argparse
@@ -7,7 +6,13 @@ import xmltodict, json
 from datetime import datetime
 from libs.examples.solution_printer import SolutionPrinter
 from libs.examples.solution_builder import SolutionBuilder
-from libs.model.task_scheduling import MinDurationModel, MinCostModel, MinResourcesModel
+from libs.model.task_scheduling_multiopt import TaskSchedulingMultiOpt
+from libs.model.solver_params import SolverParams
+
+
+INPUT_JSON = './tmp/input.json'
+OUTPUT_JSON = './tmp/out.json'
+RES_DIR = './results'
 
 def get_obj_dict(obj):
     return obj.__dict__
@@ -39,7 +44,7 @@ def clear_task_name(task_name):
         return task_name
 
 def process_json(args):
-    data = json.load(open(args.input_file), object_hook=lambda d: SimpleNamespace(**d))
+    data = json.load(open(INPUT_JSON), object_hook=lambda d: SimpleNamespace(**d))
 
     tasks = []
     for t in data.Project.Tasks.Task:
@@ -71,14 +76,10 @@ def process_json(args):
 
     pprint(resources)
 
-    fixed_assignments = []
-
-    model = MinDurationModel(resources, algo_tasks, fixed_assignments)
-    # model = MinCostModel(resources, algo_tasks, fixed_assignments)
-    # model = MinResourcesModel(resources, algo_tasks, fixed_assignments)
+    s_params = SolverParams.default()
+    s_params.max_iteration_search_time = args.max_time
+    model = TaskSchedulingMultiOpt(resources, algo_tasks, opt_mode=args.mode_list, solver_params=s_params)
     solution = model.solve()
-
-    print(solution)
 
     printer = SolutionPrinter()
     print('\nTask assignments:')
@@ -87,43 +88,39 @@ def process_json(args):
         'Resources assignments:')
     printer.print_workers_tasks(tasks, resources, solution['workers_assignments'])
 
-
     builder = SolutionBuilder()
     builder.reassign(data, tasks, resources, solution['task_assignments'])
-   
+    builder.update_parents_dates(data)
 
-    with open(args.output_file, 'w', encoding='utf-8') as f:
+    with open(OUTPUT_JSON, 'w', encoding='utf-8') as f:
         json.dump(data, f, default=get_obj_dict, ensure_ascii=False, indent=4)
 
-    with open(args.output_file[:-5] + '.xml', 'w', encoding='utf-8') as f:
-        xml_content = xmltodict.unparse(json.load(open(args.output_file)), pretty=True)
+    with open(args.output_file, 'w', encoding='utf-8') as f:
+        xml_content = xmltodict.unparse(json.load(open(OUTPUT_JSON)), pretty=True)
         f.write(xml_content)
 
 def convert_xml_to_json(args):
     with open(args.input_file) as fd:
         doc = xmltodict.parse(fd.read())
 
-    with open(args.output_file, 'w', encoding='utf-8') as f:
-        json.dump(doc, f, ensure_ascii=False, indent=4)
+    with open(INPUT_JSON, 'w', encoding='utf-8') as f:
+        json.dump(doc, f, ensure_ascii=False, indent = 4)
 
-# python main.py -i "./inputs/new/исходные данные.xml" -o "./inputs/new/исходные данные.json" -c 1
-# python main.py -i "./inputs/new/исходные данные.json" -o out.json
-
-# python main.py -i "./inputs/v2/тестовое задание.xml" -o "./inputs/v2/тестовое данные.json" -c 1
-# python main.py -i "./inputs/v2/тестовое данные.json" -o out2.json
+# export PYTHONPATH=/home/vladimir/Work/microbo/hack-it-purple-2024
+# python main.py -i "./inputs/v2/тестовое задание.xml" -o ./results/result_2.xml -m duration
+# python main.py -i "./inputs/new/исходные данные.xml" -o ./results/result_1.xml -m duration
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Sber Tech Scheduler')
+    parser = argparse.ArgumentParser(description='Sber Tech Task Scheduler')
     parser.add_argument('-i', '--input-file', type=str,
-                        help='input file', required=True)
+                        help='input xml file', required=True)
     parser.add_argument('-o', '--output-file', type=str,
-                        help='output file', required=True)
-    parser.add_argument("-c", '--convert-xml', required=False)
+                        help='output xml file', required=True)
+    parser.add_argument('-t', '--max-time', type=int, default=600,
+                        help='Max iteration search time in seconds')
+    parser.add_argument('-m', '--mode-list', nargs='+', type=str, required=True,
+                        help='duration, cost, resources')
 
     args = parser.parse_args()
-    
-    if args.convert_xml is not None:
-        pprint("convert xml")
-        convert_xml_to_json(args)
-    else:
-        pprint("process json")
-        process_json(args)
+
+    convert_xml_to_json(args)
+    process_json(args)
